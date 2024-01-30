d68338411ebdc6937044eea4de254a7051c69b53
Noh Ji Won <121555613+JTechhub@users.noreply.github.com>
2024-01-31 00:52:29 +0900

    Update main.py

--- nlp/FastAPI/preprocess+bertopic/main.py
+++ nlp/FastAPI/preprocess+bertopic/main.py
@@ -29,22 +29,24 @@ class TextData(BaseModel):
 def preprocess_data(start_date: str = Query(None), end_date: str = Query(None)):
     if not start_date or not end_date:
         raise HTTPException(status_code=400, detail="Start date and end date must be provided")
+    
     # preprocessed_data = preprocess.preprocess_data(start_date, end_date)  # 전처리 메서드 호출
     preprocessed_data = preprocess.preprocess_data(20231201, 20231201)    # 테스트용
-    data_processor = DataProcessor(tagger=mecab, engine=engine, n=2)  # DataProcessor 인스턴스 생성
+    
+    # 전처리된 데이터를 데이터베이스에서 조회
+    query = "SELECT * FROM preprocessed_news"
+    df = pd.read_sql_query(sql=query, con=engine)
+    
+    # DataProcessor 인스턴스 생성- db에 저장할 필요없음, DataProcessor와 TopicModeler를 분리하면 안됨.
+    data_processor = DataProcessor(tagger=mecab, engine=engine, n=2)  
+    # TopicModeler 인스턴스 생성, 이 때 tokenizer로 data_processor를 넘겨줌, bertopic자체에서 토큰화 진행됨. db 저장하려면 모델 다 돌리고 저장해야함.
+    topic_modeler = TopicModeler(dataprocessor=data_processor, max_features=3000)
+
+    # 'text' 열의 데이터를 fit_model에 입력
+    topic_model = topic_modeler.fit_model(df['text'])
+    
     return {"message": "Data preprocessed successfully", "data": preprocessed_data}
 
-@app.post("/analyze/")
-async def analyze_text(data: TextData):
-    # 데이터를 DataFrame으로 변환
-    df = pd.DataFrame([data.dict()])
-
-    # 토픽 모델링 수행
-    try:
-        models = train_topic_model(df)
-        return models
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
 @app.get("/")
 async def home():
