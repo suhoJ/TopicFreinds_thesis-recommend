63e6dd0e987f626808528675ff02967b209ec1f7
Noh Ji Won <121555613+JTechhub@users.noreply.github.com>
2024-01-30 17:03:36 +0900

    Update main.py

--- nlp/FastAPI/preprocess+bertopic/main.py
+++ nlp/FastAPI/preprocess+bertopic/main.py
@@ -1,1 +1,27 @@
+from fastapi import FastAPI, HTTPException
+from preprocess import DataProcessor
+import pandas as pd
+import sqlalchemy
+import logging
 
+# Update this with your MySQL RDS credentials
+db_username = "goorm"
+db_password = "goorm1415"
+db_host = "thesis-mysql.cx2cmy4wc806.ap-northeast-2.rds.amazonaws.com"
+db_name = "mydatabase"
+mysql_connection_string = f"mysql+mysqlconnector://{db_username}:{db_password}@{db_host}/{db_name}"
+
+# Create a database engine
+engine = sqlalchemy.create_engine(mysql_connection_string)
+
+app = FastAPI()
+
+
+
+@app.get("/")
+async def home():
+    return "Welcome to the preprocess App!"
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8002)
