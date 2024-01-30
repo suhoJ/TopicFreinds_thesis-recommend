45d9b70063f755ba20ee690ffc5d30bf3acd62cc
Noh Ji Won <121555613+JTechhub@users.noreply.github.com>
2024-01-30 17:02:10 +0900

    Update bertopic_model4.py

--- nlp/FastAPI/preprocess+bertopic/bertopic_model4.py
+++ nlp/FastAPI/preprocess+bertopic/bertopic_model4.py
@@ -17,7 +17,7 @@ def train_topic_model(df):
         category_docs = df[df['category'] == category]['tokenized_text_mc']
 
     # 파라미터 조정 - 더 일반적인 토픽을 위한 설정
-    topic_model_general = BERTopic(embedding_model="sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens",
+    topic_model = BERTopic(embedding_model="sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens",
                                   vectorizer_model=vectorizer,
                                   ctfidf_model=ctfidf_model,
                                   top_n_words=5,
@@ -26,7 +26,7 @@ def train_topic_model(df):
                                   calculate_probabilities=True,
                                   verbose=True)
 
-    topics_general, _ = topic_model_general.fit_transform(category_docs)
+    topics, _ = topic_model.fit_transform(category_docs)
     print(f"Category: {category} - General Topics")
     general_topic_info = topic_model_general.get_topic_info()  # 결과를 변수에 저장
     # print(general_topic_info)
