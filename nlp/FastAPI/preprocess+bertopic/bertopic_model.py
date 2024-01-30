ee2d2e549d06c25f1f6c1890a47fecd3a8f56368
Noh Ji Won <121555613+JTechhub@users.noreply.github.com>
2024-01-30 17:55:50 +0900

    Update bertopic_model.py

--- nlp/FastAPI/preprocess+bertopic/bertopic_model.py
+++ nlp/FastAPI/preprocess+bertopic/bertopic_model.py
@@ -4,7 +4,40 @@ from bertopic.vectorizers import ClassTfidfTransformer
 from gensim.models import CoherenceModel
 from sklearn.feature_extraction.text import CountVectorizer
 from konlpy.tag import Mecab
+class DataProcessor:
+    def __init__(self, tagger, engine, n=2):
+        self.engine = engine
+        self.n = n
+        self.tagger = tagger
+        self.preprocessed_news_data = None
+        self.stopwords = ['에게', '통해', '각각', '때문', '무단', '따른', '기자', '는데', '저작', '뉴스', '특파원', '하다', '이번', '이상',
+             '전년', '제품', '업체', '기업', '지난해', '대비', '올해', '의원', '내년도', '절반', '당기', '대표', '만나', '분기',
+             '국민', '정부', '지역', '현수막', '비중', '포토', 'vs', '파렴치','오전', '오후','정보', '이날', '상품', '세계',
+             '시장', '경제', '과학', '사회', '문화', '정치', '처음', '가능', '매출', '소재', '작품', '자신', '위치', 
+             '수준', '라이프', '여파', '해석', '고객', '국내', '관련', '내년', 'gb gb', '주요']   # 계속 변동 예정
+        self.correction_dict = {'lg' : '엘지', '중신' : '중신용', 'rd': 'R&D', '생활건강': '엘지생활건강', '예술전당': '예술의전당', '엘지생활':'엘지생활건강'}
+        
+   def __call__(self, text):
+        text = text[:1000000]     # 텍스트 길이 제한(메모리용량 절약)
+        raw_pos_tagged = self.tagger.pos(text)
+        word_cleaned = []
+        for word, tag in raw_pos_tagged:
+            if tag in ['NNG', "SL"] and (len(word) != 1) and (word not in self.stopwords):
+                word_cleaned.append(word)
 
+        # n-gram 생성(bigram)
+        ngrams = zip(*[word_cleaned[i:] for i in range(self.n)])
+        ngram_list = ["".join(ngram) for ngram in ngrams]  # 띄어쓰기 없이 단어를 연결
+
+        # 단어 대체(짤린 단어들 보완)
+        corrected_ngram_list = []
+        for ngram in ngram_list:
+            for wrong, correct in self.correction_dict.items():
+                ngram = ngram.replace(wrong, correct)
+            corrected_ngram_list.append(ngram)
+
+        return corrected_ngram_list
+       
 class TopicModeler:
     def __init__(self, tokenizer, max_features=3000):
         self.vectorizer = CountVectorizer(tokenizer=custom_tokenizer, max_features=max_features)
