f3e67963277ed8b44b807c5901260f6b55041192
Noh Ji Won <121555613+JTechhub@users.noreply.github.com>
2024-01-31 11:10:42 +0900

    Update main.py

--- nlp/FastAPI/research(paper)_connect/main.py
+++ nlp/FastAPI/research(paper)_connect/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
+from typing import List
 # from news_crawler import start_crawling
 from preprocess import DataProcessor
 from bertopic_model import TopicModeler, apply_category_models
@@ -19,14 +20,13 @@ import logging
 
 app = FastAPI()
 
-@app.get("/kci")
-async def kci_api():
-    try:
-        get_api(keyword)   #사용자가 키워드로 검색 시 
-        return {"status": "success", "message": "Topic modeling completed."}
-    except Exception as e:
-        logging.error(f"An error occurred in the modeling route: {e}")
-        raise HTTPException(status_code=500, detail=str(e))
+def search_papers(search_term: str) -> List[str]:
+    results = get_api(search_term)  # 실제 검색 API를 호출하고 결과를 저장
+    return results  # 실제 검색 결과를 반환
+
+@app.get("/search_papers")
+async def get_search_results(search_term: str = Query(...)):
+    return search_papers(search_term)
 
 @app.get("/")
 async def home():
