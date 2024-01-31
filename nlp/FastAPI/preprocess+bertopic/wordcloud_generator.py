2ee6ca7c3fe7caada60d25a46aae6243b55d393d
Noh Ji Won <121555613+JTechhub@users.noreply.github.com>
2024-01-31 21:15:01 +0900

    Update wordcloud_generator.py

--- nlp/FastAPI/preprocess+bertopic/wordcloud_generator.py
+++ nlp/FastAPI/preprocess+bertopic/wordcloud_generator.py
@@ -2,7 +2,12 @@ from wordcloud import WordCloud
 import io
 import base64
 
-def generate_wordcloud(topic_model, font_path='/path/to/your/font.ttf'):
+def generate_wordcloud(topic_model, font_path='/path/to/your/font.ttf'):  # 폰트 경로 설
+    wc_keywords = []
+    for topic_number, topic_keywords in topic_model.get_topics().items():
+        if topic_number != -1 and topic_number <= 15:
+             wc_keywords.append((topic_number+1, topic_keywords))
+    
     keyword_freq = {}
     for _, topic in topic_model.get_topics().items():
         for word, weight in topic:
