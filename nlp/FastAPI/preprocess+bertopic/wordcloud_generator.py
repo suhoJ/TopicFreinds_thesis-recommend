f1033050bd24963a7db1e8832d021354e3e31dfb
Noh Ji Won <121555613+JTechhub@users.noreply.github.com>
2024-01-31 20:46:58 +0900

    Update wordcloud_generator.py

--- nlp/FastAPI/preprocess+bertopic/wordcloud_generator.py
+++ nlp/FastAPI/preprocess+bertopic/wordcloud_generator.py
@@ -2,13 +2,13 @@ from wordcloud import WordCloud
 import io
 import base64
 
-def generate_wordcloud(topic_model):
+def generate_wordcloud(topic_model, font_path='/path/to/your/font.ttf'):
     keyword_freq = {}
     for _, topic in topic_model.get_topics().items():
         for word, weight in topic:
             keyword_freq[word] = keyword_freq.get(word, 0) + weight
 
-    wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(keyword_freq)
+    wordcloud = WordCloud(width=800, height=400, background_color='white', font_path=font_path).generate_from_frequencies(keyword_freq)
     img = io.BytesIO()
     wordcloud.to_image().save(img, 'PNG')
     img.seek(0)
