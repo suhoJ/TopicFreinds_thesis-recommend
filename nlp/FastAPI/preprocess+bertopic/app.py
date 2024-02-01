44e8998ecd40323cc82e824beb5ce0fce4828956
Noh Ji Won <121555613+JTechhub@users.noreply.github.com>
2024-02-01 15:13:52 +0900

    Add files via upload

---
+++ nlp/FastAPI/preprocess+bertopic/app.py
@@ -0,0 +1,28 @@
+import requests
+import streamlit as st
+from PIL import Image
+import base64
+import io
+
+# URL of your FastAPI application
+preprocess_url = "http://0.0.0.0:8002/preprocess"
+
+# Example of making a POST request to your FastAPI app (modify as needed)
+response = requests.post(preprocess_url, json=[{"text": "example text", "category": "example category"}])
+if response.status_code == 200:
+    data = response.json()
+    for category, details in data.items():
+        st.header(f"Category: {category}")
+
+        # Display top keywords
+        top_keywords = details['top_keywords']
+        st.subheader("Top Keywords:")
+        for topic_number, keyword, weight in top_keywords:
+            st.write(f"Topic {topic_number}: {keyword}")
+
+        # Display word cloud
+        wordcloud_img = details['wordcloud']
+        image = Image.open(io.BytesIO(base64.b64decode(wordcloud_img)))
+        st.image(image, caption=f"Word Cloud for {category}")
+else:
+    st.error("Failed to fetch data from API")
