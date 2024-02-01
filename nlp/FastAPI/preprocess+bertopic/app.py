43c996bae0f5f1d488bc0191f22deb950b361b64
Noh Ji Won <121555613+JTechhub@users.noreply.github.com>
2024-02-01 15:16:04 +0900

    Update app.py

--- nlp/FastAPI/preprocess+bertopic/app.py
+++ nlp/FastAPI/preprocess+bertopic/app.py
@@ -8,7 +8,7 @@ import io
 preprocess_url = "http://0.0.0.0:8002/preprocess"
 
 # Example of making a POST request to your FastAPI app (modify as needed)
-response = requests.post(preprocess_url, json=[{"text": "example text", "category": "example category"}])
+response = requests.post(preprocess_url, json=[{"text": "text", "category": "category"}])
 if response.status_code == 200:
     data = response.json()
     for category, details in data.items():
